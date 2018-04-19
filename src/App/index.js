972c7708b7e9f6e0e9c7c3608511bc35b5ef40da
roy <ruoqi.liu.nz@gmail.com>
2018-04-20 02:32:27 +1200

    prepare to pass the needed props for message part.

--- src/App/index.js
+++ src/App/index.js
@@ -4,12 +4,16 @@ import Cockpit from './Cockpit'
 import Conversations from './Conversations'
 import FriendsList from './FriendsList'
 
-const App = () => (
-  <div>
-    <FriendsList/>
-    <Conversations/>
-    <Cockpit/>
-  </div>
-)
+class App extends React.Component {
+
+  render = () => (
+    <div>
+      <FriendsList/>
+      <Conversations/>
+      <Cockpit/>
+    </div>
+  )
+}
+
 
 export default App;
